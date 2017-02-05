d7f7ffcdde490502db9e76346aaf274b8e46ba4e
acolletti21 <ashleymcolletti@gmail.com>
2017-02-05 13:46:34 -0600

    added practice ideas

--- crud-practice-01/db/migrate/20170129195245_change_locaton_to_location.rb
+++
@@ -1,5 +0,0 @@
-class ChangeLocatonToLocation < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :restaurants, :locaton, :location
-  end
-end
---
+++ hobby_count.rb
@@ -0,0 +1,2 @@
+#make a hash of users that have multiple hobbies and multiple favorite colors, count how many things they have in common
+
